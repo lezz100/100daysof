ec462f742e9ad3b510690ab3bd7c328c62efab46
lezz100 <preciouskipkorir374@gmail.com>
2025-05-16 20:02:06 +0300

    more commits

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+public class ReverseString {
+    public static void main(String[] args){
+        System.out.println("Dog");
+
+       String r = reverse("Dog");
+       System.out.println(r);   
+    }
+
+    public static String reverse(String s) {
+        char [] letters = new char [s.length()];
+        int letterIndex = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            letters[letterIndex] = s.charAt(i);
+            letterIndex++;
+            System.out.println(s.charAt(i));
+        }
+
+        String reverse = "";
+        for (int i = 0; i < s.length(); i++) {
+            reverse += letters[i];
+        }
+        return reverse;
+    }
+}
